a049679e916ffdcc7b45a7cb1570d8e9bd8c3534
johyunkang <javajo82@gmail.com>
2022-06-11 16:59:08 +0900

    login dialog

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import { Button } from '@material-ui/core';
+import { Button, Modal, makeStyles, Input } from '@material-ui/core';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post'
 
 const BASE_URL = 'http://localhost:8000/'
 
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        position: 'absolute',
+        width: 400,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3)
+    }
+}))
+
 function App() {
+    const classes = useStyles();
 
     const [posts, setPosts] = useState([]);
     const [openSignIn, setOpenSignIn] = useState(false);
     const [openSignUp, setOpenSignUp] = useState(false);
+    const [modalStyle, setModalStyle] = useState(getModalStyle)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
 
     useEffect(() => {
         fetch(BASE_URL + 'post/all')
@@ -28,7 +55,6 @@ function App() {
                     const d_a = new Date(Date.UTC(t_a[0], t_a[1]-1, t_a[2], t_a[3], t_a[4], t_a[5]));
                     const d_b = new Date(Date.UTC(t_b[0], t_b[1]-1, t_b[2], t_b[3], t_b[4], t_b[5]));
                     return d_b - d_a // for Reverse order
-
                 })
                 return result
             })
@@ -41,9 +67,44 @@ function App() {
             })
     }, [])
 
+    const signIn = (event) => {
+        
+    }
+
 
     return (
         <div className="app">
+
+            <Modal
+                open={openSignIn}
+                onClose={() => setOpenSignIn(false)}>
+
+                <div style={modalStyle} className={classes.paper}>
+                    <form className="app_signin">
+                        <center>
+                            <img className='app_headerImage'
+                                    src="https://cdn.ibos.kr/design/upload_file/__HTMLEDITOR__/moosa73/0sp2n4ndfl7e6ldliffnv64hv6_15125360567641.png"
+                                    alt="Instagram" />
+                        </center>
+                        <Input
+                            placeholder="username"
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)} />
+                        <Input
+                            placeholder="password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)} />
+                        <Button
+                            type="submit"
+                            onClick={signIn}>Login</Button>
+                    </form>
+
+                </div>
+
+            </Modal>
+
             <div className='app_header'>
                 <img className='app_headerImage'
                     src="https://cdn.ibos.kr/design/upload_file/__HTMLEDITOR__/moosa73/0sp2n4ndfl7e6ldliffnv64hv6_15125360567641.png"
